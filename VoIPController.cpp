3556309881ded6e8ed020b7d3e678677c45ce782
Klemens Nanni <klemens@posteo.de>
2021-12-15 18:00:20 +0100

    audio: Build on OpenBSD
    OpenBSD supports PulseAudio and OpenAL, but not ALSA.
    Either PulseAudio or ALSA are required for libtgvoip.
    
    Add missing OS macros to enable building with
    `LIBTGVOIP_DISABLE_PULSEAUDIO=ON` `LIBTGVOIP_DISABLE_ALSA=OFF` as tested
    on OpenBSD/amd64 7.2-current.

--- VoIPController.cpp
+++ VoIPController.cpp
@@ -3014,7 +3014,7 @@ static void initMachTimestart() {
 #endif
 
 double VoIPController::GetCurrentTime(){
-#if defined(__linux__) || defined(__FreeBSD__)
+#if defined(__linux__) || defined(__FreeBSD__) || defined(__OpenBSD__)
 	struct timespec ts;
 	clock_gettime(CLOCK_MONOTONIC, &ts);
 	return ts.tv_sec+(double)ts.tv_nsec/1000000000.0;
